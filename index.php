5f6f844b557f2bb8cc7e0a0461ce7fa55c5d646a
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2019-09-26 22:06:52 +0530

    Added navigation bar

--- index.php
+++ index.php
@@ -19,64 +19,124 @@ if (isset($_SESSION['userLoggedIn'])) {
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-	
-	<div id="nowPlayingBarContainer">
-		<div id="nowPlayingBar">
+
+	<div id="mainContainer">
+		<div id="topContainer">
 			
+			<div id="navBarContainer">
+				<nav class="navBar">
 
-			<div id="nowPlayingLeft">
-				<div class="content">
-					<span class="albumLink">
-						<img src="assets/images/square.png" class="albumArtwork">
-					</span>
+					<a href="index.php" class="logo">
+						<img src="assets/images/icons/logo.png">
+					</a>
 
-					<div class="trackInfo">
-						<span class="trackName">
-							<span>Happy Birthday</span>
-						</span>
-
-						<span class="artistName">
-							<span>Eric Stanley</span>
-						</span>
+					<div class="group">
+						
+						<div class="navItem">
+							<a href="search.php" class="navItemLink">Search
+								<img src="assets/images/icons/search.png" class="icon" alt="Search">
+							</a>
+						</div>
 
 					</div>
 
-				</div>
+					<div class="group">
+						<div class="navItem">
+							<a href="browse.php" class="navItemLink">Browse</a>
+						</div>
+
+						<div class="navItem">
+							<a href="yourMusic.php" class="navItemLink">Your Music</a>
+						</div>
+
+						<div class="navItem">
+							<a href="profile.php" class="navItemLink">Eric Stanley</a>
+						</div>
+					</div>
+
+				</nav>
 			</div>
 
-			<div id="nowPlayingCenter">
+		</div>
 
-				<div class="content playerControls">
+		<div id="nowPlayingBarContainer">
+			<div id="nowPlayingBar">
+				
 
-					<div class="buttons">
-						<button class="controlButton shuffle" title="Shuffle">
-							<img src="assets/images/icons/shuffle.png" alt="Shuffle">
-						</button>
+				<div id="nowPlayingLeft">
+					<div class="content">
+						<span class="albumLink">
+							<img src="assets/images/square.png" class="albumArtwork">
+						</span>
 
-						<button class="controlButton previous" title="Previous">
-							<img src="assets/images/icons/previous.png" alt="Previous">
-						</button>
+						<div class="trackInfo">
+							<span class="trackName">
+								<span>Happy Birthday</span>
+							</span>
 
-						<button class="controlButton play" title="Play">
-							<img src="assets/images/icons/play.png" alt="Play">
-						</button>
+							<span class="artistName">
+								<span>Eric Stanley</span>
+							</span>
 
-						<button class="controlButton pause" title="Pause" style="display: none;">
-							<img src="assets/images/icons/pause.png" alt="Pause">
-						</button>
+						</div>
 
-						<button class="controlButton next" title="Next">
-							<img src="assets/images/icons/next.png" alt="Next">
-						</button>
-
-						<button class="controlButton repeat" title="Repeat">
-							<img src="assets/images/icons/repeat.png" alt="Repeat">
-						</button>
 					</div>
+				</div>
 
-					<div class="playbackBar">
+				<div id="nowPlayingCenter">
+
+					<div class="content playerControls">
+
+						<div class="buttons">
+							<button class="controlButton shuffle" title="Shuffle">
+								<img src="assets/images/icons/shuffle.png" alt="Shuffle">
+							</button>
+
+							<button class="controlButton previous" title="Previous">
+								<img src="assets/images/icons/previous.png" alt="Previous">
+							</button>
+
+							<button class="controlButton play" title="Play">
+								<img src="assets/images/icons/play.png" alt="Play">
+							</button>
+
+							<button class="controlButton pause" title="Pause" style="display: none;">
+								<img src="assets/images/icons/pause.png" alt="Pause">
+							</button>
+
+							<button class="controlButton next" title="Next">
+								<img src="assets/images/icons/next.png" alt="Next">
+							</button>
+
+							<button class="controlButton repeat" title="Repeat">
+								<img src="assets/images/icons/repeat.png" alt="Repeat">
+							</button>
+						</div>
+
+						<div class="playbackBar">
+							
+							<span class="progressTime current">0.00</span>
+
+							<div class="progressBar">
+								<div class="progressBarBg">
+									<div class="progress"></div>
+								</div>
+							</div>
+
+							<span class="progressTime remaining">0.00</span>
+
+						</div>
 						
-						<span class="progressTime current">0.00</span>
+					</div>
+					
+				</div>
+
+				<div id="nowPlayingRight">
+					<div class="volumeBar">
+						
+						<button class="controlButton volume" title="Volume button">
+							<img src="assets/images/icons/volume.png" alt="Volume">
+						</button>
 
 						<div class="progressBar">
 							<div class="progressBarBg">
@@ -84,30 +144,10 @@ if (isset($_SESSION['userLoggedIn'])) {
 							</div>
 						</div>
 
-						<span class="progressTime remaining">0.00</span>
-
 					</div>
-					
 				</div>
-				
+
 			</div>
-
-			<div id="nowPlayingRight">
-				<div class="volumeBar">
-					
-					<button class="controlButton volume" title="Volume button">
-						<img src="assets/images/icons/volume.png" alt="Volume">
-					</button>
-
-					<div class="progressBar">
-						<div class="progressBarBg">
-							<div class="progress"></div>
-						</div>
-					</div>
-
-				</div>
-			</div>
-
 		</div>
 	</div>
 
