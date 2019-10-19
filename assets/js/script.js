45bcdd22672a9802f29d364e1d6079551d73f9dc
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2019-10-19 13:35:56 +0530

    Added settings page

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,37 @@ $(document).on("change", "select.playlist", function() {
 
 });
 
+function updateEmail(emailClass) {
+	var emailValue = $("." + emailClass).val();
+
+	$.post("includes/handlers/ajax/updateEmail.php", { email: emailValue, username: userLoggedIn })
+		.done(function(response) {
+			$("." + emailClass).nextAll(".message").text(response);
+		});
+}
+
+function updatePassword(oldPasswordClass, newPasswordClass1, newPasswordClass2) {
+	var oldPassword = $("." + oldPasswordClass).val();
+	var newPassword1 = $("." + newPasswordClass1).val();
+	var newPassword2 = $("." + newPasswordClass2).val();
+
+	$.post("includes/handlers/ajax/updatePassword.php", 
+		{ oldPassword: oldPassword, 
+			newPassword1: newPassword1,
+			newPassword2: newPassword2,
+			username: userLoggedIn })
+
+		.done(function(response) {
+			$("." + oldPasswordClass).nextAll(".message").text(response);
+		});
+}
+
+function logout() {
+	$.post("includes/handlers/ajax/logout.php", function() {
+		location.reload();
+	});
+}
+
 function openPage(url) {
 
 	if (timer != null) {
