f0787300c5aa923241dcb633fceb30ff5940a610
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2020-05-04 20:01:30 +0530

    Migrated from mySQL to ClearDB

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,15 @@
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("localhost", "root", "", "spotify");
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$con = new mysqli_connect($server, $username, $password, $db);
+	// $con = mysqli_connect("localhost", "root", "", "spotify");
 
 	if (mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
